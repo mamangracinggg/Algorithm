164657ef037ca6ce576f1ac19681b351260a7f03
yoseph yusanto <120454307+mamangracinggg@users.noreply.github.com>
2023-09-29 07:44:51 +0700

    Create O(N).cpp

---
+++ Prime Number/O(N).cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 2; i <= n - 1; i++){
+        if(n % i == 0){
+            cout<<"Bukan Prima"<<endl;
+          return 0;
+        }
+    }
+    cout<<"Prima"<<endl;
+    return 0;
+}
